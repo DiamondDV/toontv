5df6c9c705b65ee248112805bd1943e1d589f846
Divyansh <divyanshvgs0@gmail.com>
2025-11-09 13:25:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10087607
+    "zoneId": 10163860
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
